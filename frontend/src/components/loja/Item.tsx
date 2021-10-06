304a6be24bc22d47f64806b790acb0009f3fa6bd
danilo-lescano <nq3i4fsx@hotmail.com>
2021-10-05 22:20:09 -0300

    ++

--- frontend/src/components/loja/Item.tsx
+++ frontend/src/components/loja/Item.tsx
@@ -60,9 +60,9 @@ const Carrousel = (values: {imgs: string[] | undefined}) => {
 
     return (
         <div className={'carrousel-box'}>
-            <div className={'carrousel-inner-box'} style={{width: 'calc( 100% *' + carrouselImages.length + ' )', transform: 'translateX(-' + 100/carrouselImages.length*indexCurentImage + '%)', height: 'calc( 100vw * 0.5625)'}}>
+            <div className={'carrousel-inner-box'} style={{width: 'calc( 100% *' + carrouselImages.length + ' )', transform: 'translateX(-' + 100/carrouselImages.length*indexCurentImage + '%)'}}>
                 {Object.values(carrouselImages).map((image) =>
-                    <span style={{width: 'calc( 100% /' + carrouselImages.length + ' )', height: 'calc( 100vw * 0.5625)'}}>
+                    <span style={{width: 'calc( 100% /' + carrouselImages.length + ' )'}}>
                         <img key={image} src={image} className={'carrousel-inner-box-img'}/>
                     </span>
                 )}
