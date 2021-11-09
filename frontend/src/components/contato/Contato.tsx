82fe05047985ed7d6b58a9d08c5abe22cfc18677
danilo-lescano <nq3i4fsx@hotmail.com>
2021-11-08 22:02:27 -0300

    ++

--- frontend/src/components/contato/Contato.tsx
+++ frontend/src/components/contato/Contato.tsx
@@ -1,6 +1,21 @@
 const Contato = function () {
     return (
-        <h1>Contato</h1>
+        <div className={"wrapper"}>
+            <h1 className={"texto-vazado"}>CONTATO</h1>
+            <div className={'texto-comum'}>Entre em contato por What's App<br/>no número <span style={{fontWeight: 'bold'}}><em>+55 67 984247615</em></span></div>
+            <a href={'https://wa.me/5567984247615'} target={'_blank'}>
+                <button className={'clickable entrar-em-contato-btn'}>ENTRAR EM CONTATO</button>
+            </a>
+            <div className={'texto-comum'}>ou</div>
+            <div className={'texto-comum'} style={{margin: '50px 0'}}>Envie um e-mail:</div>
+            <input type={"email"} className={'left-navbar-input'} placeholder={'Email'} style={{fontSize: '1.2em'}}/>
+            <div style={{margin: '30px 0 15px 0', fontSize: '1.8em', fontWeight: 'lighter'}}>Mensagem</div>
+            <div className={'text-area-wrapper'}>
+                <textarea className={'text-area'}/>
+                <br/>
+                <button className={'clickable btn-de-enviar'}>ENVIAR</button>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/sobre/Sobre.tsx
+++ frontend/src/components/sobre/Sobre.tsx
@@ -1,7 +1,50 @@
+import imgbg from '../../assets/madeira-cortada.png';
+import parceiro1 from '../../assets/parceiros_logos_Prancheta 1.svg';
+import parceiro2 from '../../assets/parceiros_logos-02.svg';
+import parceiro3 from '../../assets/parceiros_logos-03.svg';
+import parceiro4 from '../../assets/parceiros_logos-04.svg';
+import parceiro5 from '../../assets/parceiros_logos-05.svg';
+
 const Sobre = function () {
-    return (
-        <h1>Sobre</h1>
-    )
+    return (<>
+        <div className={"wrapper"} style={{fontSize: '.8em'}}>
+            <h1 className={"texto-vazado"}>SOBRE</h1>
+            <div className={'texto-comum'} style={{marginBottom: 30}}>Somos um estúdio de arquitetura, design e fabricação<br/>digital, localizado em Capo Grande-MS</div>
+            <div className={'texto-comum'} style={{marginBottom: 30}}>Nossa produção é divida em duas áreas: O desenvolvimento<br/>de produtos e a prestação de serviços.</div>
+            <div className={'texto-comum'} style={{marginBottom: 30}}>A partir da fabricação digital pretendemos desenvolver<br/>uma linha de mobiliários minimalistas, autênticos por<br/>terem como diferencial:</div>
+            <div className={'texto-comum'} style={{marginBottom: 30, fontWeight: 'bold', fontFamily: 'SerifaStd'}}><em>
+                - Um design autoral e cuidadoso<br/>
+                - Rápida produção<br/>
+                - Rápida montagem e facilidade de transpoorte<br/>
+                - Uma abordagem menos nociva ao meio ambiente<br/>
+            </em></div>
+            <div className={'texto-comum'} style={{marginBottom: 60}}>Para saber mais visite nosso <em style={{fontWeight: 'bold'}}>Instagram</em></div>
+            <h1 className={"texto-vazado"}>APOIO</h1>
+            <div>
+                <div className={'imagem-sobre-apoio'}>
+                    <img src={parceiro1}/>
+                </div>
+                <div className={'imagem-sobre-apoio'}>
+                    <img src={parceiro2}/>
+                </div>
+                <div className={'imagem-sobre-apoio'}>
+                    <img src={parceiro3}/>
+                </div>
+                <div className={'imagem-sobre-apoio'}>
+                    <img src={parceiro4}/>
+                </div>
+            </div>
+            <h1 className={"texto-vazado"}>PARCERIA</h1>
+            <div>
+                <div className={'imagem-sobre-apoio'}>
+                    <img src={parceiro5}/>
+                </div>
+            </div>
+        </div>
+        <div className={'carrousel-box'} style={{marginTop: 100}}>
+            <img src={imgbg} style={{width: '100%'}}/>
+        </div>
+    </>)
 }
 
 export default Sobre;
